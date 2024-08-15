b769c4b4bf77c8501d6f2584a0b82e5671168b0f
CwqSS <168303452+CwqSS@users.noreply.github.com>
2024-08-15 17:07:37 -0300

    Atualizar o q86.c
    Corrigindo pequeno erro

--- strings/q86.c
+++ strings/q86.c
@@ -80,7 +80,7 @@ int main()
         {
             proxNumero = 1000;
         }
-        else if (caracter == '\0')
+        else if (proxCaracter == '\0')
         {
             proxNumero = 0;
         }
