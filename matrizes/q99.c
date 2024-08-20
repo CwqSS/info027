b5f0cb03c95d34b0be9c30d484aed88f8d2c84e2
CwqSS <168303452+CwqSS@users.noreply.github.com>
2024-08-20 19:21:11 -0300

    Update q99.c
    Questão feita após dica do professor.

--- matrizes/q99.c
+++ matrizes/q99.c
@@ -1,46 +1,70 @@
 #include <stdio.h>
-#define TAM 512
-#define True 1
-#define False 0
+#define TAM 12
 
 int main()
 {
-    int matriz[TAM][2];
-    int iCont, jCont, kCont, idadeValida = True;
-    int frequencia, aux, resposta[2], combinacaoIdade[2];
+    int matriz[TAM][TAM];
+    int idadeHomem, idadeMulher, frequenciaHomem, frequenciaMulher, frequenciaCasal[2];
+    int iCont, jCont, kCont, idadeValida = 0;
+    int aux, frequencia = 0;
     
-    for(iCont = 0; iCont < TAM && idadeValida; iCont++)
-        for(jCont = 0; jCont < 2 && idadeValida; jCont++)
-        {
-            scanf("%d", &matriz[iCont][jCont]);
-            if (matriz[iCont][jCont] < 18 || matriz[iCont][jCont] > 30)
-            {
-                matriz[iCont][jCont] = 0;
-                idadeValida = False;
-            }
-        }
+    for(iCont = 0; iCont < TAM; iCont++)
+        for(jCont = 0; jCont < TAM; jCont++)
+            matriz[iCont][jCont] = 0;
     
-    for(jCont = 0; jCont < 2; jCont++)
+    do
     {
-        frequencia = 0;
-        for(kCont = 18; kCont <= 30; kCont++)
+        puts("Informe a idade do casal (H M):");
+        scanf("%d %d", &idadeHomem, &idadeMulher);
+        if(idadeHomem >= 18 && idadeHomem <= 30 && idadeMulher >= 18 && idadeMulher <= 30)
         {
-            aux = 0;
-            idadeValida = True;
-            for(iCont = 0; iCont < TAM && idadeValida; iCont++)
-            {
-                if(kCont == matriz[iCont][jCont])
-                {
-                    aux += 1;
-                    if(aux > frequencia)
-                    {
-                        frequencia = aux;
-                        resposta[jCont] = kCont;
-                    }
-                }
-                if(matriz[iCont][jCont] < 18 || matriz[iCont][jCont] > 30)
-                    idadeValida = False;
-            }
+            iCont = idadeHomem - 18;
+            jCont = idadeMulher - 18;
+            matriz[iCont][jCont] += 1;
+            idadeValida += 1;
         }
+        else
+            iCont = jCont = -1;
+    } while(iCont != -1);
+    
+    for(iCont = 0; iCont < TAM; iCont++)
+    {
+        aux = 0;
+        for(jCont = 0; jCont < TAM; jCont++)
+            aux += matriz[iCont][jCont];
+        if(aux > frequencia)
+            frequencia = iCont + 18;
+    }
+    frequenciaHomem = frequencia;
+    
+    for(jCont = 0; jCont < TAM; jCont++)
+    {
+        aux = 0;
+        for(iCont = 0; iCont < TAM; iCont++)
+            aux += matriz[iCont][jCont];
+        if(aux > frequencia)
+            frequencia = jCont + 18;
+    }
+    frequenciaMulher = frequencia;
+    
+    frequencia = 0;
+    for(iCont = 0; iCont < TAM; iCont++)
+        for(jCont = 0; jCont < TAM; jCont++)
+            if(matriz[iCont][jCont] > frequencia)
+            {
+                frequencia = matriz[iCont][jCont];
+                frequenciaCasal[0] = iCont + 18;
+                frequenciaCasal[1] = jCont + 18;
+            }
+    
+    if(idadeValida > 0)
+    {
+        printf("A idade de homem mais frequente é: %d\n", frequenciaHomem);
+        printf("A idade de mulher mais frequente é: %d\n", frequenciaMulher);
+        printf("A idade de homem mais frequente é: %d para homens e %d para mulheres\n", frequenciaCasal[0], frequenciaCasal[1]);
+    }
+    else
+    {
+        printf("Informe ao menos um valor.");
     }
 }
